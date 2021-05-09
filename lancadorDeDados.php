ec9364fa30bbff7bc82174200c96c270a2c1c141
Matheus Candido <matheusfc77@hotmail.com>
2021-05-09 09:34:11 -0300

    Versão de Teste 1
    Adição das imagens e melhora na interação com o usuário.

--- lancadorDeDados.php
+++ lancadorDeDados.php
@@ -5,7 +5,15 @@
     $titulo = "Lançador de Dados";
     $paragrafo1 = 
         "Teste sua sorte com os dados! Só não vale trapacear!";
-    $textoAcao = ""
+    $textoAcao = "";
+    $dados = array(
+        1 => "imagens/l1.png",
+        2 => "imagens/l2.png",
+        3 => "imagens/l3.png",
+        4 => "imagens/l4.png",
+        5 => "imagens/l5.png",
+        6 => "imagens/l6.png",
+    );
 ?>
 <html>
 <head>
@@ -37,17 +45,63 @@
             $lancComp2 = rand(1, 6);
             $totUsuario = $lancUsuario1 + $lancUsuario2;
             $totComp = $lancComp1 + $lancComp2;
-
-            echo "<br>L1 U: ".$lancUsuario1."<br>";
-            echo "L2 U: ".$lancUsuario2."<br>";
-            echo "L1 C: ".$lancComp1."<br>";
-            echo "L2 C: ".$lancComp2."<br>";
-            echo "TT U: ".$totUsuario."<br>";
-            echo "TT C: ".$totComp."<br>";
         }
-
-        //echo $textoAcao;
     ?>
+
+    <br/>
+
+    <div class="user">
+        <h3>Seus Lançamentos</h3>
+        
+        <div class="box">
+            <img src=
+                <?php echo "\"".$dados[$lancUsuario1]."\""; ?>/>
+            <p>Dado 1</p>
+        </div>
+        <div class="box">
+            <img src=<?php echo "\"".$dados[$lancUsuario2]."\""; ?> hspace="100"/>
+            <p style="text-indent:100px;">Dado 2</p>
+        </div>
+
+        <h4><?php echo "Total: ".$totUsuario." pontos."; ?></h4>
+    </div>
+
+    <br/>
+
+    <div class="computer">
+        <h3>Computador</h3>
+        
+        <div class="box">
+            <img src=<?php echo "\"".$dados[$lancComp1]."\""; ?>/>
+            <p>Dado 1</p>
+        </div>
+        <div class="box">
+            <img src=<?php echo "\"".$dados[$lancComp2]."\""; ?> hspace="100"/>
+            <p style="text-indent:100px;">Dado 2</p>
+        </div>
+
+        <h4><?php echo "Total: ".$totComp." pontos."; ?></h4>
+    </div>
+
+    <br/>
+
+    <h2>
+        <?php
+            if ($totUsuario > $totComp) 
+                echo "Você Venceu! Parece que está com sorte. Jogue de novo para ganhar mais prêmios!";
+            else if ($totUsuario == $totComp) 
+                echo "Empate! Dessa vez passou perto. Tente de novo para vencer!";
+            else
+                echo "Você perdeu! Não desanime, aposto que os dados serão melhores na próxima!" 
+        ?>
+    </h2>
     
+    <style>
+        div.box {
+            width: 150px;
+            display: inline-block;
+        }
+    </style>
+
 </body>
 </html>
